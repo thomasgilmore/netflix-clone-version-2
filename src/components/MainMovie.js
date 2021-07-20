c4c302373835d3961a7a9b0e41cf75b631cdbce3
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-07-20 11:49:28 -0400

    Lazy Load pictures for Main Picture and all picture cards

--- src/components/MainMovie.js
+++ src/components/MainMovie.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './mainmovie.css';
 import { GrPlayFill } from "react-icons/gr";
 import { AiOutlineInfoCircle } from "react-icons/ai";
+import { LazyLoadImage  } from 'react-lazy-load-image-component';
 
 export default function MainMovie(props) {
         return (
           <div className="imageContainer">
-            <img src={props.movieBackdrop} alt="Movie or TV Show Back Drop" className="mainMovie" />
+            <LazyLoadImage src={props.movieBackdrop} alt="Movie or TV Show Back Drop" className="mainMovie" />
             <div className="movieTitle"><h1>{props.movieTitle}</h1></div>
             <div className="buttonMain">
               <button className="btn btn-light playButton" type="button"><GrPlayFill className="playIcon" /><span className="playText"> Play</span></button>
