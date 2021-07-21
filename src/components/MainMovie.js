78d16e5c1707304606a1dad242dd92bf30e32d5c
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-07-21 12:14:46 -0400

    added height to image container

--- src/components/MainMovie.js
+++ src/components/MainMovie.js
@@ -6,7 +6,7 @@ import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 export default function MainMovie(props) {
         return (
-          <div className="imageContainer" width="100%" max-height="600px">
+          <div className="imageContainer" width="100%" height="600px">
             <LazyLoadImage src={props.movieBackdrop} alt="Movie or TV Show Back Drop" className="mainMovie" width="100%" max-height="600px" />
             <div className="movieTitle"><h1>{props.movieTitle}</h1></div>
             <div className="buttonMain">
